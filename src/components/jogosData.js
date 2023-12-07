2618bd2b48eee8a8dcab2dc65f548665043c594d
andrephelipe <andre.phelipe.1999@gmail.com>
2023-12-07 12:56:02 -0500

    jogos de quinta e sexta

--- src/components/jogosData.js
+++ src/components/jogosData.js
@@ -1,39 +1,3 @@
-import brasil from '../assets/brasil.png'
-import nicaragua from '../assets/nicaragua.png'
-import goias from '../assets/goias.png'
-import americamg from '../assets/americamg.png'
-import fluminense from '../assets/fluminense.png'
-import gremio from '../assets/gremio.png'
-import vasco from '../assets/vasco.png'
-import bragantino from '../assets/bragantino.png'
-import saopaulo from '../assets/saopaulo.png'
-import flamengo from '../assets/flamengo.png'
-import santos from '../assets/santos.png'
-import fortaleza from '../assets/fortaleza.png'
-import cruzeiro from '../assets/cruzeiro.png'
-import palmeiras from '../assets/palmeiras.png'
-import internacional from '../assets/internacional.png'
-import botafogo from '../assets/botafogo.png'
-import coritiba from '../assets/coritiba.png'
-import corithians from '../assets/corinthians.png'
-import bahia from '../assets/bahia.png'
-import atleticomg from '../assets/atleticomg.png'
-import cuiaba from '../assets/cuiaba.png'
-import athleticopr from '../assets/athleticopr.png'
-import brighton from '../assets/brighton.png'
-import brentford from '../assets/brentford.png'
-import sheffieldunited from '../assets/sheffieldunited.png'
-import liverpool from '../assets/liverpool.png'
-import crystalpalace from '../assets/crystalpalace.png'
-import bournemouth from '../assets/bournemouth.png'
-import fulham from '../assets/fulham.png'
-import nottinghamforest from '../assets/nottinghamforest.png'
-import astonvila from '../assets/astonvila.png'
-import manchestercity from '../assets/manchestercity.png'
-import manchesterunited from '../assets/manchesterunited.png'
-import chelsea from '../assets/chelsea.png'
-import olympiquedemarselha from '../assets/olympiquedemarselha.png'
-import lyon from '../assets/lyon.png'
 import everton from '../assets/everton.png'
 import newcastle from '../assets/newcastle.png'
 import tottenham from '../assets/tottenham.png'
@@ -48,204 +12,6 @@ import napoli from '../assets/napoli.png'
 export const jogosData = [
   {
     id: 1,
-    time1: 'Brasil Feminino',
-    time2: 'Nicaragua Feminino',
-    vencedor: null,
-    logo1: brasil,
-    logo2: nicaragua,
-    odds1: 1.30,
-    oddsEmpate: 2.0,
-    odds2: 2.50,
-  },
-  {
-    id: 2,
-    time1: 'Goias',
-    time2: 'America-MG',
-    vencedor: null,
-    logo1: goias,
-    logo2: americamg,
-    odds1: 1.90,
-    oddsEmpate: 3.75,
-    odds2: 3.80,
-  },
-  {
-    id: 3,
-    time1: 'Fluminense',
-    time2: 'Gremio',
-    vencedor: null,
-    logo1: fluminense,
-    logo2: gremio,
-    odds1: 2.05,
-    oddsEmpate: 3.75,
-    odds2: 3.30,
-  },
-  {
-    id: 4,
-    time1: 'Vasco',
-    time2: 'Bragantino',
-    vencedor: null,
-    logo1: vasco,
-    logo2: bragantino,
-    odds1: 2.10,
-    oddsEmpate: 3.50,
-    odds2: 3.50,
-  },
-  {
-    id: 5,
-    time1: 'Sao Paulo',
-    time2: 'Flamengo',
-    vencedor: null,
-    logo1: saopaulo,
-    logo2: flamengo,
-    odds1: 3.0,
-    oddsEmpate: 3.40,
-    odds2: 2.37,
-  },
-  {
-    id: 6,
-    time1: 'Santos',
-    time2: 'Fortaleza',
-    vencedor: null,
-    logo1: santos,
-    logo2: fortaleza,
-    odds1: 1.90,
-    oddsEmpate: 3.30,
-    odds2: 4.50,
-  },
-  {
-    id: 7,
-    time1: 'Cruzeiro',
-    time2: 'Palmeiras',
-    vencedor: null,
-    logo1: cruzeiro,
-    logo2: palmeiras,
-    odds1: 3.10,
-    oddsEmpate: 2.90,
-    odds2: 2.62,
-  },
-  {
-    id: 8,
-    time1: 'Internacional',
-    time2: 'Botafogo',
-    vencedor: null,
-    logo1: internacional,
-    logo2: botafogo,
-    odds1: 2.20,
-    oddsEmpate: 3.25,
-    odds2: 3.50,
-  },
-  {
-    id: 9,
-    time1: 'Coritiba',
-    time2: 'Corinthians',
-    vencedor: null,
-    logo1: coritiba,
-    logo2: corithians,
-    odds1: 3.10,
-    oddsEmpate: 3.20,
-    odds2: 2.40,
-  },
-  {
-    id: 10,
-    time1: 'Bahia',
-    time2: 'Atletico-MG',
-    vencedor: null,
-    logo1: bahia,
-    logo2: atleticomg,
-    odds1: 2.40,
-    oddsEmpate: 3.50,
-    odds2: 2.70,
-  },
-  {
-    id: 11,
-    time1: 'Cuiaba',
-    time2: 'Athletico-PR',
-    vencedor: null,
-    logo1: cuiaba,
-    logo2: athleticopr,
-    odds1: 2.20,
-    oddsEmpate: 3.25,
-    odds2: 3.50,
-  },
-  {
-    id: 12,
-    time1: 'Brighton',
-    time2: 'Brentford',
-    vencedor: null,
-    logo1: brighton,
-    logo2: brentford,
-    odds1: 1.85,
-    oddsEmpate: 3.80,
-    odds2: 3.80,
-  },
-  {
-    id: 13,
-    time1: 'Sheffield United FC',
-    time2: 'Liverpool',
-    vencedor: null,
-    logo1: sheffieldunited,
-    logo2: liverpool,
-    odds1: 13.0,
-    oddsEmpate: 7.50,
-    odds2: 1.16,
-  },
-  {
-    id: 14,
-    time1: 'Crystal Palace',
-    time2: 'Bournemouth',
-    vencedor: null,
-    logo1: crystalpalace,
-    logo2: bournemouth,
-    odds1: 2.15,
-    oddsEmpate: 3.50,
-    odds2: 3.30,
-  },
-  {
-    id: 15,
-    time1: 'Fulham',
-    time2: 'Nottingham Forest',
-    vencedor: null,
-    logo1: fulham,
-    logo2: nottinghamforest,
-    odds1: 1.95,
-    oddsEmpate: 3.50,
-    odds2: 3.80,
-  },
-  {
-    id: 16,
-    time1: 'Aston Villa',
-    time2: 'Manchester City',
-    vencedor: null,
-    logo1: astonvila,
-    logo2: manchestercity,
-    odds1: 4.50,
-    oddsEmpate: 4.20,
-    odds2: 1.70,
-  },
-  {
-    id: 17,
-    time1: 'Manchester United',
-    time2: 'Chelsea',
-    vencedor: null,
-    logo1: manchesterunited,
-    logo2: chelsea,
-    odds1: 2.8,
-    oddsEmpate: 3.50,
-    odds2: 2.40,
-  },
-  {
-    id: 18,
-    time1: 'Olympique de Marselha',
-    time2: 'Lyon',
-    vencedor: null,
-    logo1: olympiquedemarselha,
-    logo2: lyon,
-    odds1: 1.75,
-    oddsEmpate: 3.80,
-    odds2: 4.50,
-  },
-  {
-    id: 19,
     time1: 'Everton',
     time2: 'Newcastle',
     vencedor: null,
@@ -256,7 +22,7 @@ export const jogosData = [
     odds2: 2.25,
   },
   {
-    id: 20,
+    id: 2,
     time1: 'Tottenham',
     time2: 'West Ham',
     vencedor: null,
@@ -267,7 +33,7 @@ export const jogosData = [
     odds2: 4.20,
   },
   {
-    id: 21,
+    id: 3,
     time1: 'Getafe',
     time2: 'Valencia',
     vencedor: null,
@@ -278,7 +44,7 @@ export const jogosData = [
     odds2: 2.80,
   },
   {
-    id: 22,
+    id: 4,
     time1: 'Hoffenheim',
     time2: 'Bochum',
     vencedor: null,
@@ -289,7 +55,7 @@ export const jogosData = [
     odds2: 5.0,
   },
   {
-    id: 23,
+    id: 5,
     time1: 'Juventus',
     time2: 'Napoli',
     vencedor: null,
