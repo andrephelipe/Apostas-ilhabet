06d0877627900735954dbf04ee64e0bfaca9e345
andrephelipe <andre.phelipe.1999@gmail.com>
2023-12-09 11:22:57 -0500

    jogos de sabado

--- src/components/jogosData.js
+++ src/components/jogosData.js
@@ -1,81 +1,316 @@
-import getafe from '../assets/getafe.png'
-import valencia from '../assets/valencia.png'
-import hoffenheim from '../assets/hoffenheim.png'
-import bochum from '../assets/bochum.png'
-import juventus from '../assets/juventus.png'
-import napoli from '../assets/napoli.png'
-import montpellier from '../assets/montpellier.png'
-import lens from '../assets/lens.png'
-import fcvizela from '../assets/fcvizela.png'
-import braga from '../assets/braga.png'
-import benfica from '../assets/benfica.png'
-import scfarense from '../assets/scfarense.png'
+import crystalpalace from '../assets/crystalpalace.png'
+import liverpool from '../assets/liverpool.png'
+import brighton from '../assets/brighton.png'
+import burnley from '../assets/burnley.png'
+import manchesterunited from '../assets/manchesterunited.png'
+import afcbournemouth from '../assets/afcbournemouth.png'
+import sheffieldunitedfc from '../assets/sheffieldunitedfc.png'
+import brentfordfc from '../assets/brentfordfc.png'
+import wolverhamptonwanderers from '../assets/wolverhamptonwanderers.png'
+import nottinghamforest from '../assets/nottinghamforest.png'
+import astonvila from '../assets/astonvila.png'
+import arsenal from '../assets/arsenal.png'
+import hellasverona from '../assets/hellasverona.png'
+import lazio from '../assets/lazio.png'
+import atalanta from '../assets/atalanta.png'
+import milan from '../assets/milan.png'
+import inter from '../assets/inter.png'
+import udinese from '../assets/udinese.png'
+import rennes from '../assets/rennes.png'
+import asmonaco from '../assets/asmonaco.png'
+import psg from '../assets/psg.png'
+import nantes from '../assets/nantes.png'
+import deportivoalaves from '../assets/deportivoalaves.png'
+import udlaspalmas from '../assets/udlaspalmas.png'
+import realbetis from '../assets/realbetis.png'
+import realmadrid from '../assets/realmadrid.png'
+import villarreal from '../assets/villarreal.png'
+import realsociedad from '../assets/realsociedad.png'
+import rcdmallorca from '../assets/rcdmallorca.png'
+import sevilla from '../assets/sevilla.png'
+import heidenheim from '../assets/heidenheim.png'
+import darmstadt from '../assets/darmstadt.png'
+import unionberlin from '../assets/unionberlin.png'
+import borussiamonchengladbach from '../assets/borussiamonchengladbach.png'
+import eintrachtfrankfurt from '../assets/eintrachtfrankfurt.png'
+import bayerndemunique from '../assets/bayerndemunique.png'
+import werderbremen from '../assets/werderbremen.png'
+import augsburg from '../assets/augsburg.png'
+import wolfsburgo from '../assets/wolfsburgo.png'
+import friburgo from '../assets/friburgo.png'
+import borussiadortmund from '../assets/borussiadortmund.png'
+import rbleipzig from '../assets/rbleipzig.png'
+import portimonensefc from '../assets/portimonensefc.png'
+import fcfamalicao from '../assets/fcfamalicao.png'
+import vitoriasc from '../assets/vitoriasc.png'
+import sporting from '../assets/sporting.png'
+import fcporto from '../assets/fcporto.png'
+import casapiaac from '../assets/casapiaac.png'
+
 
 export const jogosData = [
   {
     id: 1,
-    time1: 'Juventus',
-    time2: 'Napoli',
+    time1: 'Crystal Palace',
+    time2: 'Liverpool',
     vencedor: null,
-    logo1: juventus,
-    logo2: napoli,
-    odds1: 2.25,
-    oddsEmpate: 3.25,
-    odds2: 3.25,
+    logo1: crystalpalace,
+    logo2: liverpool,
+    odds1: 6.00,
+    oddsEmpate: 4.50,
+    odds2: 1.50,
   },
   {
     id: 2,
-    time1: 'Montpellier',
-    time2: 'Lens',
+    time1: 'Brighton',
+    time2: 'Burnley',
     vencedor: null,
-    logo1: montpellier,
-    logo2: lens,
-    odds1: 3.50,
-    oddsEmpate: 3.40,
-    odds2: 2.15,
+    logo1: brighton,
+    logo2: burnley,
+    odds1: 1.50,
+    oddsEmpate: 4.75,
+    odds2: 5.50,
   },
   {
     id: 3,
-    time1: 'Getafe',
-    time2: 'Valencia',
+    time1: 'Manchester United',
+    time2: 'AFC Bournemouth',
     vencedor: null,
-    logo1: getafe,
-    logo2: valencia,
-    odds1: 2.62,
-    oddsEmpate: 3.0,
-    odds2: 2.80,
+    logo1: manchesterunited,
+    logo2: afcbournemouth,
+    odds1: 1.55,
+    oddsEmpate: 4.33,
+    odds2: 5.75,
   },
   {
     id: 4,
-    time1: 'Hoffenheim',
-    time2: 'Bochum',
+    time1: 'Sheffield United FC',
+    time2: 'Brentford FC',
     vencedor: null,
-    logo1: hoffenheim,
-    logo2: bochum,
-    odds1: 1.65,
-    oddsEmpate: 4.20,
-    odds2: 4.75,
+    logo1: sheffieldunitedfc,
+    logo2: brentfordfc,
+    odds1: 4.20,
+    oddsEmpate: 3.75,
+    odds2: 1.80,
   },
   {
     id: 5,
-    time1: 'FC Vizela',
-    time2: 'Braga',
+    time1: 'Wolverhampton Wanderers',
+    time2: 'Nottingham Forest',
     vencedor: null,
-    logo1: fcvizela,
-    logo2: braga,
-    odds1: 5.25,
-    oddsEmpate: 4.33,
-    odds2: 1.57,
+    logo1: wolverhamptonwanderers,
+    logo2: nottinghamforest,
+    odds1: 1.85,
+    oddsEmpate: 3.60,
+    odds2: 4.20,
   },
   {
     id: 6,
-    time1: 'Benfica',
-    time2: 'SC Farense',
+    time1: 'Aston Villa',
+    time2: 'Arsenal FC',
     vencedor: null,
-    logo1: benfica,
-    logo2: scfarense,
-    odds1: 1.16,
-    oddsEmpate: 8.50,
+    logo1: astonvila,
+    logo2: arsenal,
+    odds1: 3.30,
+    oddsEmpate: 3.75,
+    odds2: 2.05,
+  },
+  {
+    id: 7,
+    time1: 'Hellas Verona',
+    time2: 'Lazio',
+    vencedor: null,
+    logo1: hellasverona,
+    logo2: lazio,
+    odds1: 4.00,
+    oddsEmpate: 3.40,
+    odds2: 1.95,
+  },
+  {
+    id: 8,
+    time1: 'Atalanta',
+    time2: 'Milan',
+    vencedor: null,
+    logo1: atalanta,
+    logo2: milan,
+    odds1: 2.50,
+    oddsEmpate: 3.20,
+    odds2: 2.90,
+  },
+  {
+    id: 9,
+    time1: 'Inter',
+    time2: 'Udinese',
+    vencedor: null,
+    logo1: inter,
+    logo2: udinese,
+    odds1: 1.25,
+    oddsEmpate: 6.50,
+    odds2: 9.50,
+  },
+  {
+    id: 10,
+    time1: 'Rennes',
+    time2: 'AS Monaco',
+    vencedor: null,
+    logo1: rennes,
+    logo2: asmonaco,
+    odds1: 2.55,
+    oddsEmpate: 3.60,
+    odds2: 2.62,
+  },
+  {
+    id: 11,
+    time1: 'PSG',
+    time2: 'Nantes',
+    vencedor: null,
+    logo1: psg,
+    logo2: nantes,
+    odds1: 1.20,
+    oddsEmpate: 7.50,
     odds2: 12.0,
+  },
+  {
+    id: 12,
+    time1: 'Deportivo Alaves',
+    time2: 'UD Las Palmas',
+    vencedor: null,
+    logo1: deportivoalaves,
+    logo2: udlaspalmas,
+    odds1: 2.05,
+    oddsEmpate: 3.00,
+    odds2: 4.20,
+  },
+  {
+    id: 13,
+    time1: 'Real Betis',
+    time2: 'Real Madrid',
+    vencedor: null,
+    logo1: realbetis,
+    logo2: realmadrid,
+    odds1: 4.20,
+    oddsEmpate: 3.80,
+    odds2: 1.80,
+  },
+  {
+    id: 14,
+    time1: 'Villarreal',
+    time2: 'Real Sociedad',
+    vencedor: null,
+    logo1: villarreal,
+    logo2: realsociedad,
+    odds1: 3.40,
+    oddsEmpate: 3.40,
+    odds2: 2.10,
+  },
+  {
+    id: 15,
+    time1: 'RCD Mallorca',
+    time2: 'Sevilla',
+    vencedor: null,
+    logo1: rcdmallorca,
+    logo2: sevilla,
+    odds1: 2.70,
+    oddsEmpate: 3.10,
+    odds2: 2.80,
+  },
+  {
+    id: 16,
+    time1: 'Heidenheim',
+    time2: 'Darmstadt',
+    vencedor: null,
+    logo1: heidenheim,
+    logo2: darmstadt,
+    odds1: 2.05,
+    oddsEmpate: 3.50,
+    odds2: 3.50,
+  },
+  {
+    id: 17,
+    time1: 'Union Berlin',
+    time2: 'Borussia Monchengladbach',
+    vencedor: null,
+    logo1: unionberlin,
+    logo2: borussiamonchengladbach,
+    odds1: 2.10,
+    oddsEmpate: 3.50,
+    odds2: 3.40,
+  },
+  {
+    id: 18,
+    time1: 'Eintracht Frankfurt',
+    time2: 'Bayern de Munique',
+    vencedor: null,
+    logo1: eintrachtfrankfurt,
+    logo2: bayerndemunique,
+    odds1: 8.50,
+    oddsEmpate: 6.00,
+    odds2: 1.30,
+  },
+  {
+    id: 19,
+    time1: 'Werder Bremen',
+    time2: 'Augsburg',
+    vencedor: null,
+    logo1: werderbremen,
+    logo2: augsburg,
+    odds1: 2.36,
+    oddsEmpate: 3.75,
+    odds2: 2.75,
+  },
+  {
+    id: 20,
+    time1: 'Wolfsburgo',
+    time2: 'Friburgo',
+    vencedor: null,
+    logo1: wolfsburgo,
+    logo2: friburgo,
+    odds1: 2.25,
+    oddsEmpate: 3.60,
+    odds2: 3.00,
+  },
+  {
+    id: 21,
+    time1: 'Borussia Dortmund',
+    time2: 'RB Leipzig',
+    vencedor: null,
+    logo1: borussiadortmund,
+    logo2: rbleipzig,
+    odds1: 2.50,
+    oddsEmpate: 3.90,
+    odds2: 2.50,
+  },
+  {
+    id: 22,
+    time1: 'Portimonense SC',
+    time2: 'FC Famalicao',
+    vencedor: null,
+    logo1: portimonensefc,
+    logo2: fcfamalicao,
+    odds1: 2.90,
+    oddsEmpate: 3.25,
+    odds2: 2.45,
+  },
+  {
+    id: 23,
+    time1: 'Vitoria S.C.',
+    time2: 'Sporting',
+    vencedor: null,
+    logo1: vitoriasc,
+    logo2: sporting,
+    odds1: 5.25,
+    oddsEmpate: 4.00,
+    odds2: 1.61,
+  },
+  {
+    id: 24,
+    time1: 'FC Porto',
+    time2: 'Casa Pia AC',
+    vencedor: null,
+    logo1: fcporto,
+    logo2: casapiaac,
+    odds1: 1.25,
+    oddsEmpate: 6.00,
+    odds2: 10.00,
   },
 ];
